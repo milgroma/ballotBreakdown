de261c576814551860d2c5a533fb4ee609d325ba
milgroma <milgrom.adam@gmail.com>
2014-12-06 12:15:50 -0500

    upgraded to rails 4.1

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module BallotBreakdown
   class Application < Rails::Application
@@ -19,8 +19,5 @@ module BallotBreakdown
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
-    config.assets.initialize_on_precompile = false
-    
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-BallotBreakdown::Application.config.session_store :cookie_store, key: '_ballotBreakdown_session'
+Rails.application.config.session_store :cookie_store, key: '_ballot_breakdown_session'
